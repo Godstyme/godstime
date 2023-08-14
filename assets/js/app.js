c2fe1743ff5352f02300acfc486c27f2cca801dd
Godstime Onyibe <godstimeonyibe@gmail.com>
2023-08-14 18:07:40 +0100

    Implemented a download button and refactor the social icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,16 @@
 const currentYear = new Date().getFullYear()
-document.querySelector('#tradeMarkTime').innerHTML = currentYear
+document.querySelector('#tradeMarkTime').innerHTML = currentYear;
+
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+function startDownload() {
+  window.open("https://drive.google.com/file/d/13_VaDowHUqnw2iM2T2BGibpLIMeAqxSu/view?usp=drive_link")
+}
